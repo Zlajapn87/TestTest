0d099842538088cb268691348aaf7f92348573a9
Zlajapn87 <Zlaja2018>
2021-09-17 07:23:05 +0200

    Initial commit

---
+++ src/CharacterCount.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class CharacterCount {
+
+   // Brojanje karaktera
+   public static int counter(String sKeyboardInput){
+       String mesasge = sKeyboardInput;
+       int n = sKeyboardInput.length();
+       return n;
+   }
+
+   //Spajanje Stringova
+   public static String addingString(String message) {
+       String initialMessage = message;
+       System.out.println("Unesite drugu poruku: ");
+       Scanner am = new Scanner(System.in);
+       String additionalMessage;
+       additionalMessage = am.nextLine();
+       return initialMessage + additionalMessage;
+   }
+
+   // Generisanje random maila
+
+    public static String mailGenerator() {
+       String email = "";
+       Random rnd = new Random();
+       System.out.print(rnd);
+       email = rnd + "@gmail.com";
+       return email;
+    }
+
+
+
+}
---
+++ src/mainClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class mainClass {
+
+    public static void main(String[] args) {
+
+        String sCharacters;
+        String sJointMessages;
+        Scanner sInput = new Scanner(System.in);
+        sCharacters = sInput.nextLine();
+
+        //Inicijalna poruka i brojenje karaktera;
+        System.out.println("Iniciajlna poruka je: " + sCharacters + ", i ima " + CharacterCount.counter(sCharacters) + " karaktera :)" );
+
+
+        //Nastavak druge na prvu poruku i brojenje karaktera;
+
+        sJointMessages = CharacterCount.addingString(sCharacters);
+        System.out.println("Zbirna poruka je: " + sJointMessages + ", a ukupan broj karaktera u zbirnoj poruci je: " + CharacterCount.counter(sJointMessages) + "\n Hvala lepo i dovidjenja");
+
+        // Ispisivanje random mail-a
+        System.out.println("Random generisan email: " + CharacterCount.mailGenerator());
+
+    }
+}
